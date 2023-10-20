d7b12fa9c6638f39c8d97670976d7ea32f0edad0
tarla <tarlavina50@mail.ru>
2023-10-20 18:19:31 +0700

    Add project files.

---
+++ WinApp/WinApI.cpp
@@ -0,0 +1,16 @@
+#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	MessageBox
+	(
+		NULL, 
+		"Hello Windows!\nПривет ...", 
+		"Header(Caption) Заголовок", 
+		MB_YESNOCANCEL | MB_ICONINFORMATION | MB_HELP |
+		MB_DEFBUTTON3  |
+		MB_SYSTEMMODAL //|
+		//MB_RIGHT       | MB_RTLREADING
+	);
+	return 0;
+}
