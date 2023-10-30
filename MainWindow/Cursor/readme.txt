c711c18dda41ae3efdf3d6188319d649138dd602
tarla <tarlavina50@mail.ru>
2023-10-30 18:23:14 +0700

    Coding class

---
+++ MainWindow/Cursor/readme.txt
@@ -0,0 +1,21 @@
+﻿=== Kagamine Rin Cursor Set ===
+
+By: ko ❤ (http://www.rw-designer.com/user/114989) sabah.jannat.n@gmail.com
+
+Download: http://www.rw-designer.com/cursor-set/rin-kagamine
+
+Author's description:
+
+Vocaloid Rin Kagamine! (all animations and images by me)
+
+Updated: 23/10/2023
+ ・added person select
+ ・added location select
+
+==========
+
+License: Released to Public Domain
+
+You are free:
+
+* To use this work for any legal purpose.
--- MainWindow/MainWindow.cpp
+++ MainWindow/MainWindow.cpp
@@ -4,6 +4,10 @@
 #include<stdio.h>
 #include"resource.h"
 
+#define IDC_COMBO 1001
+#define IDC_BUTTON_APPLY 1002
+CONST CHAR* g_CURSOR[] = { "busy.ani", "Working In Background.ani", "normal.ani","move.ani"};
+
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Mt Window Class";//им€ класса окна
 
@@ -26,7 +30,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.hIcon = (HICON)LoadImage(hInstance, "macaroon.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hIcon = (HICON)LoadImage(hInstance, "donut.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	//wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hCursor = (HCURSOR)LoadImage(hInstance, "cursors.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hCursor = (HCURSOR)LoadImage(hInstance, "cursors.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
 
 	wc.hInstance = hInstance; //hInstance - экзаемпл€р исполн€емого файла программы в пам€ти
@@ -91,11 +95,72 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 	}
 	case WM_CREATE:
+	{
+		HWND hCombo = CreateWindowEx
+
+		(NULL,
+			"ComboBox",
+			"",
+			WS_CHILD | WS_VISIBLE | CBN_DROPDOWN,
+			10, 10,
+			200, 200,
+			hwnd,
+			(HMENU)IDC_COMBO,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		for (int i = 0; i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+		}
+		HWND bButton = CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"Apply",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			220, 10,
+			100, 24,
+			hwnd,
+			(HMENU)IDC_BUTTON_APPLY,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
 		break;
+
 	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_APPLY:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_filename[_MAX_FNAME]{};
+			CHAR sz_filepath[_MAX_PATH] = "Cursor\\";
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+			strcat(sz_filepath, sz_filename);
+			//MessageBox(hwnd, sz_filename, "Info", MB_OK);
+			HCURSOR hCursor = (HCURSOR)LoadImage(
+				GetModuleHandle(NULL),
+				sz_filepath,
+				IMAGE_CURSOR,
+				LR_DEFAULTSIZE, LR_DEFAULTSIZE,
+				LR_LOADFROMFILE);
+			//SetCursorPos(500, 200);
+			SetClassLong(hwnd, GCL_HCURSOR, (LONG)hCursor);
+			SetClassLong(GetDlgItem(hwnd, IDC_BUTTON_APPLY), GCL_HCURSOR, (LONG)hCursor);
+			SetClassLong(GetDlgItem(hwnd, IDC_COMBO), GCL_HCURSOR, (LONG)hCursor);
+			//SetCursor(hCursor);
+			return FALSE;
+		}
+			break;
+		}
+	}
 		break;
 	case WM_DESTROY: PostQuitMessage(0); break;
-	case WM_CLOSE: DestroyWindow(hwnd); break;
+	case WM_CLOSE:   DestroyWindow(hwnd); break;
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return NULL;
