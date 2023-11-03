6c23a44397d7a58e0875b4c132eace3d7ca2358e
tarla <tarlavina50@mail.ru>
2023-11-03 21:24:22 +0700

    Coding class

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -66,7 +66,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,
 		g_sz_CLASSNAME,
 		g_sz_CLASSNAME,
-		WS_OVERLAPPEDWINDOW,//Окно является перекрывающимся. Перекрывающееся окно имеет заголовок окна и границу. 
+		WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX,//Окно является перекрывающимся. Перекрывающееся окно имеет заголовок окна и границу. //^ - убирает флаги
+		//WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		(BUTTON_SIZE + INTERVAL) * 5 + BUTTON_START_X * 3, SCREEN_HEIGTH + (BUTTON_SIZE + INTERVAL) * 5 + INTERVAL * 2,
 		NULL,
@@ -222,8 +223,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		(
 			NULL, "Button", "C",
 			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
-			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y,
-			BUTTON_SIZE, BUTTON_DOUBLE_SIZE,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y + BUTTON_SIZE + INTERVAL,
+			BUTTON_SIZE, BUTTON_SIZE,
 			hwnd,
 			(HMENU)(IDC_BUTTON_CLEAR),
 			GetModuleHandle(NULL),
@@ -240,11 +241,22 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
+		CreateWindowEx//backspace
+		(
+			NULL, "Button", "<-",
+			WS_CHILDWINDOW | WS_VISIBLE | BS_PUSHBUTTON,
+			BUTTON_START_X + (BUTTON_SIZE + INTERVAL) * 4, BUTTON_START_Y,
+			BUTTON_SIZE, BUTTON_SIZE,
+			hwnd,
+			(HMENU)(IDC_BUTTON_BSP),
+			GetModuleHandle(NULL),
+			NULL
+		);
 	}
 	break;
 	case WM_COMMAND:
 	{
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 		//-----------------------------------DIGIT---------------------------------------------------------------------------
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9 || LOWORD(wParam) == IDC_BUTTON_POINT)
 		{
@@ -313,6 +325,14 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			sprintf(sz_buffer, "%g", a);//"%g" - округляет
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
+		if (LOWORD(wParam) == IDC_BUTTON_BSP)
+		{
+			CHAR sz_buffer[MAX_PATH]{};
+			SendMessage(hEdit, WM_GETTEXT, MAX_PATH, (LPARAM)sz_buffer);
+			if(sz_buffer[0])sz_buffer[strlen(sz_buffer) - 1] = 0;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+
+		}
 		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
 		{
 			a = b = 0;
@@ -320,7 +340,30 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			input = false;
 			input_operation = false;
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
-
+		}
+	}
+	break;
+	case WM_KEYDOWN://Кейс назыатий с клавиатуры
+	{
+		if (GetKeyState(VK_SHIFT) < 0)
+		{
+			if (wParam == 0x38)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+		}
+		else if (LOWORD(wParam) >= 0x30 && LOWORD(wParam) <= 0x39)
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam) - 0x30 + IDC_BUTTON_0, 0);
+		if(LOWORD(wParam) >= 0x60 && LOWORD(wParam) <= 0x69)
+			SendMessage(hwnd, WM_COMMAND, LOWORD(wParam) - 0x60 + IDC_BUTTON_0, 0);
+		switch (LOWORD(wParam))
+		{
+		case VK_OEM_PLUS:   SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_PLUS, 0);  break;
+		case VK_OEM_MINUS:  SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_MINUS, 0); break;
+		case VK_MULTIPLY:   SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0); break;
+		case VK_OEM_2:
+		case VK_DIVIDE:     SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_SLASH, 0); break;
+		case VK_OEM_PERIOD: SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_POINT, 0); break;
+		case VK_RETURN:     SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0); break;
+		case VK_ESCAPE:     SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_CLEAR, 0); break;
+		case VK_BACK:       SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_BSP, 0);   break;
 		}
 	}
 	break;
