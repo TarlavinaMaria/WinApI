fb8d1c99635781cf62a9100c4c1c797dac3cc86b
tarla <tarlavina50@mail.ru>
2023-11-05 21:41:38 +0700

    Coding

--- Calculator/resource.h
+++ Calculator/resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++ generated include file.
 // Used by Calculator.rc
 //
-#define IDI_ICON1                       101
 #define IDI_CALC                        101
 #define IDC_EDIT                        999
 #define IDC_BUTTON_0                    1000
@@ -28,7 +27,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
